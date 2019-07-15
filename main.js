8e485d43170a8e0221cc487278768ff87a72c5ba
Austin Moore <37962463+Austinmoore1492@users.noreply.github.com>
2019-07-15 11:33:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-"use strict"
+const coffeeType = document.querySelector('#coffeeType');
+const submitButton = document.querySelector('#submit');
+const roastSelection = document.querySelector('#roast-selection');
+const search = document.querySelector('#search');
+
+submitButton.addEventListener('click', updateCoffees);
+search.addEventListener('keyup', searchBar);
+
 
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
+    let coffeeType = `<div class="coffee">`;
+    coffeeType += `<h3 class="coffeeHeader">${coffee.name}</h3>`;
+    coffeeType += `<h3 class="coffeeRoast">${coffee.roast}</h3>`;
+    coffeeType += `</div>`;
 
-    return html;
+    return coffeeType;
 }
 
 function renderCoffees(coffees) {
-    var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    let html = '';
+    for(let i = 0; i < coffees.length; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
@@ -20,38 +26,62 @@ function renderCoffees(coffees) {
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
-    coffees.forEach(function(coffee) {
+    const selectedRoast = roastSelection.value;
+    let filteredCoffees = [];
+    coffees.forEach((coffee) => {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
+        } else if (selectedRoast === 'All'){
+            filteredCoffees = coffees;
         }
     });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
+    coffeeType.innerHTML = renderCoffees(filteredCoffees);
+}
+
+function searchBar(e) {
+    let input = search.value.toUpperCase();
+    let searchedCoffee = [];
+
+    coffees.forEach((coffee) => {
+        if (coffee.name.toUpperCase().indexOf(input) === 0){
+            searchedCoffee.push(coffee)
+        }
+    });
+    coffeeType.innerHTML = renderCoffees(searchedCoffee);
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+const coffees = [
+    {id: 1, name: 'Light City', roast: 'Light'},
+    {id: 2, name: 'Half City', roast: 'Light'},
+    {id: 3, name: 'Cinnamon', roast: 'Light'},
+    {id: 4, name: 'City', roast: 'Medium'},
+    {id: 5, name: 'American', roast: 'Medium'},
+    {id: 6, name: 'Breakfast', roast: 'Medium'},
+    {id: 7, name: 'High', roast: 'Dark'},
+    {id: 8, name: 'Continental', roast: 'Dark'},
+    {id: 9, name: 'New Orleans', roast: 'Dark'},
+    {id: 10, name: 'European', roast: 'Dark'},
+    {id: 11, name: 'Espresso', roast: 'Dark'},
+    {id: 12, name: 'Viennese', roast: 'Dark'},
+    {id: 13, name: 'Italian', roast: 'Dark'},
+    {id: 14, name: 'French', roast: 'Dark'},
 ];
 
-var tbody = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
+coffeeType.innerHTML = renderCoffees(coffees);
 
-tbody.innerHTML = renderCoffees(coffees);
 
-submitButton.addEventListener('click', updateCoffees);
+
+//Change the text on the button when hovered
+submitButton.addEventListener("mouseover", changeText);
+submitButton.addEventListener("mouseout", changeBack);
+
+function changeText() {
+  setTimeout(() => {submitButton.innerText = "Coffee Time!!!";}, 250);
+}
+
+function changeBack() {
+  setTimeout(() => {submitButton.innerText = "Submit";}, 250);
+}
+
+
